59f30a94b401092b2476675e219d8d2a1bf3e6f6
fevi29 <fevenbayu5@gmail.com>
2024-08-30 00:02:07 +0300

    interview

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,91 +1,81 @@
 #!/usr/bin/python3
-'''N Queens Challenge'''
+""" This module solves the N-Queens problem.
+The N-Queens problem involves placing N non-attacking queens on an N×N
+chessboard. """
 
 import sys
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+def move(n):
+    """Find all solutions to the N-Queens problem.
 
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
+    Args:
+    - n (int): The size of the chessboard and the number of queens to be
+      placed.
 
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
+    Returns:
+    - list: A list of lists, each inner list representing a valid queen
+    placement on the board.
+    """
 
-    solutions = []
-    placed_queens = []  # coordinates format [row, column]
-    stop = False
-    r = 0
-    c = 0
+    result = []
+    backtrack(n, [], result)
+    return result
 
-    # iterate thru rows
-    while r < n:
-        goback = False
-        # iterate thru columns
-        while c < n:
-            # check is current column is safe
-            safe = True
-            for cord in placed_queens:
-                col = cord[1]
-                if(col == c or col + (r-cord[0]) == c or
-                        col - (r-cord[0]) == c):
-                    safe = False
-                    break
 
-            if not safe:
-                if c == n - 1:
-                    goback = True
-                    break
-                c += 1
-                continue
+def backtrack(n, path, result):
+    """Backtracking function to find valid queen placements.
 
-            # place queen
-            cords = [r, c]
-            placed_queens.append(cords)
-            # if last row, append solution and reset all to last unfinished row
-            # and last safe column in that row
-            if r == n - 1:
-                solutions.append(placed_queens[:])
-                for cord in placed_queens:
-                    if cord[1] < n - 1:
-                        r = cord[0]
-                        c = cord[1]
-                for i in range(n - r):
-                    placed_queens.pop()
-                if r == n - 1 and c == n - 1:
-                    placed_queens = []
-                    stop = True
-                r -= 1
-                c += 1
-            else:
-                c = 0
-            break
-        if stop:
-            break
-        # on fail: go back to previous row
-        # and continue from last safe column + 1
-        if goback:
-            r -= 1
-            while r >= 0:
-                c = placed_queens[r][1] + 1
-                del placed_queens[r]  # delete previous queen coordinates
-                if c < n:
-                    break
-                r -= 1
-            if r < 0:
-                break
-            continue
-        r += 1
+    Args:
+    - n (int): The size of the chessboard and the number of queens to be
+      placed.
+    - path (list): The current queen placement being explored.
+    - result (list): The list to store valid queen placements.
+    """
 
-    for idx, val in enumerate(solutions):
-        if idx == len(solutions) - 1:
-            print(val, end='')
-        else:
-            print(val)
+    if len(path) == n:
+        result.append([[i, path[i]] for i in range(n)])
+        return
+
+    for col in range(n):
+        if is_safe(col, path):
+            path.append(col)
+            backtrack(n, path, result)
+            path.pop()
+
+
+def is_safe(col, path):
+    """Check if placing a queen at a specific position is safe.
+
+    Args:
+    - col (int): The column position to check.
+    - path (list): The current queen placement being explored.
+
+    Returns:
+    - bool: True if placing a queen at the position is safe, False otherwise.
+    """
+
+    row = len(path)
+    for r, c in enumerate(path):
+        if c == col or r - c == row - col or r + c == row + col:
+            return False
+    return True
+
+
+if len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+n = sys.argv[1]
+
+try:
+    n = int(n)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+moves = move(n)
+for movement in moves:
+    print(movement)
