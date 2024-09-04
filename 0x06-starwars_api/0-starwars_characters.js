c6ca3aea85ac4e0fba130d2f69c839d739f9f20e
fevi29 <fevenbayu5@gmail.com>
2024-09-04 22:57:43 +0300

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,38 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-const movieId = process.argv[2];
-request(url + movieId, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    printInOrder(characters);
-  }
-}
-);
+// Prints all characters of a Star Wars movie using the ALX Star Wars API
 
-async function printInOrder (characters) {
-  for (const character of characters) {
-    await new Promise((resolve, reject) => {
-      request(character, function (error, response, body) {
-        if (error) {
-          reject(error);
-        } else {
-          console.log(JSON.parse(body).name);
-          resolve();
-        }
-      });
+if (process.argv.length !== 3) {
+  process.exit();
+}
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+const request = require('request');
+
+// Recursive function to get and print character names from a list of URLs
+function printNextCharacter (urls) {
+  // Remove first URL from list and parse it
+  const characterURL = urls.shift();
+  if (characterURL) {
+    request(characterURL, function (error, response, body) {
+      if (!error) {
+        // Parse and print the character name from the response body
+        console.log(JSON.parse(body).name);
+        // Move to next character in the list
+        printNextCharacter(urls);
+      }
     });
   }
 }
+
+// Fetches a list of character's URLs then uses a callback to print each one's name
+function printStarWarsCharacters (url) {
+  // Get list of URLs for characters in selected film
+  request(url, function (error, response, body) {
+    if (!error) {
+      // Call recursive function to fetch and print character names from URLs list
+      printNextCharacter(JSON.parse(body).characters);
+    }
+  });
+}
+
+printStarWarsCharacters(url);
